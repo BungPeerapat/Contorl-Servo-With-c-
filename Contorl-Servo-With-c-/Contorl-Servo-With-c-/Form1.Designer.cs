0687946970a8ff6e7ff96b90db656c37cf321ce7
BungPeerapat <mashiro74122541@gmail.com>
2020-05-30 14:34:59 +0700

    Create Contorl Servo

--- Contorl-Servo-With-c-/Contorl-Servo-With-c-/Form1.Designer.cs
+++ Contorl-Servo-With-c-/Contorl-Servo-With-c-/Form1.Designer.cs
@@ -28,13 +28,84 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.degree = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(101, 42);
+            this.trackBar1.Maximum = 180;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(558, 56);
+            this.trackBar1.TabIndex = 0;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(68, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(16, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(676, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "180";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(324, 101);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "มุม = ";
+            // 
+            // degree
+            // 
+            this.degree.AutoSize = true;
+            this.degree.Location = new System.Drawing.Point(376, 101);
+            this.degree.Name = "degree";
+            this.degree.Size = new System.Drawing.Size(16, 17);
+            this.degree.TabIndex = 3;
+            this.degree.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(758, 134);
+            this.Controls.Add(this.degree);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.trackBar1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label degree;
     }
 }
 
--- Contorl-Servo-With-c-/Contorl-Servo-With-c-/Form1.cs
+++ Contorl-Servo-With-c-/Contorl-Servo-With-c-/Form1.cs
@@ -7,14 +7,42 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO.Ports;
 
 namespace Contorl_Servo_With_c_
 {
     public partial class Form1 : Form
     {
+        SerialPort port;
         public Form1()
         {
             InitializeComponent();
+            contorlservo();
+        }
+
+        void contorlservo()
+        {
+            port = new SerialPort();
+            port.PortName = "COM3";
+            port.BaudRate = 9600;
+            try
+            {
+                port.Open();
+            }catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            degree.Text = trackBar1.Value.ToString();
+
+            if (port.IsOpen)
+            {
+
+            }
+            
         }
     }
 }
